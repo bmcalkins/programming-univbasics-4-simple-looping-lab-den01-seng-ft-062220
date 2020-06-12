0740bd4275e5aeb64a581eb21a67e1f1d94391c9
Bradley Calkins <b.raders1013@gmail.com>
2020-06-12 03:32:27 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,13 +1,11 @@
-string = ["Hello World!"]
-
-def loop_message_five_times(string)
-  loop_message_n_times(string, 5)
-end 
-
 def loop_message_n_times(string, n)
    counter = 0 
 while counter < n do
   puts string
     counter += 1
   end 
+end 
+
+def loop_message_five_times(string)
+  loop_message_n_times(string, 5)
 end 
