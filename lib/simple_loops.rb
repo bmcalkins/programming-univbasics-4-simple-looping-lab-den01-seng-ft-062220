5f760397ff9eadedf3e634373712fdaf2c34dfaa
Bradley Calkins <b.raders1013@gmail.com>
2020-06-12 03:37:33 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -9,3 +9,11 @@ end
 def loop_message_five_times(string)
   loop_message_n_times(string, 5)
 end 
+
+def output_array(array)
+    counter = 0
+  while counter < array.length do
+    puts array[counter]
+      counter += 1
+  end
+end
